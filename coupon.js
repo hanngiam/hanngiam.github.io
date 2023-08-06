d8b466eda6fa6199461034079bb4f6d0f720f983
hanngiam <141527978+hanngiam@users.noreply.github.com>
2023-08-05 22:35:20 -0400

    window.open

--- coupon.js
+++ coupon.js
@@ -4,6 +4,7 @@ var cpnCode = document.getElementById('cpnCode');
 cpnBtn.onclick = function(){
     navigator.clipboard.writeText(cpnCode.innerHTML);
     cpnBtn.innerHTML = 'COPIED';
+    window.open("https://www.w3schools.com");
     setTimeout(function(){
         cpnBtn.innerHTML  = 'COPY CODE';
         }, 3000);
