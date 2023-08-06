c2c2489d4fc2c42cb216ac2f08465f4bdd850487
hanngiam <141527978+hanngiam@users.noreply.github.com>
2023-08-05 22:40:44 -0400

    Referral link

--- coupon.js
+++ coupon.js
@@ -4,7 +4,7 @@ var cpnCode = document.getElementById('cpnCode');
 cpnBtn.onclick = function(){
     navigator.clipboard.writeText(cpnCode.innerHTML);
     cpnBtn.innerHTML = 'COPIED';
-    window.open("https://www.w3schools.com");
+    window.open("https://vaticpro.com/?sca_ref=3559290.eaFXOYkPjd");
     setTimeout(function(){
         cpnBtn.innerHTML  = 'COPY CODE';
         }, 3000);
