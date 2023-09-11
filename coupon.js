9dec0e3bb7f5c8c5299a4672c26324d70e5445e9
hanngiam <141527978+hanngiam@users.noreply.github.com>
2023-09-11 08:13:07 -0400

    Update coupon.js

--- coupon.js
+++ coupon.js
@@ -4,8 +4,8 @@ var cpnCode = document.getElementById('cpnCode');
 cpnBtn.onclick = function(){
     navigator.clipboard.writeText(cpnCode.innerHTML);
     cpnBtn.innerHTML = 'COPIED';
-    //window.open("https://vaticpro.com/?sca_ref=3559290.eaFXOYkPjd");
     setTimeout(function(){
         cpnBtn.innerHTML  = 'COPY CODE';
         }, 3000);
+    window.open(https://vaticpro.com/?sca_ref=3559290.eaFXOYkPjd','_blank');
 }
